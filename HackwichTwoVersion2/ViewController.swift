b9469da5049cc7729215cc4097b20748a9186318
CM Student <cmstudent@uhwo-acm-10.uhwo.hawaii.edu>
2020-01-28 10:48:39 -1000

    Initial Commit

---
+++ HackwichTwoVersion2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackwichTwoVersion2
+//
+//  Created by CM Student on 1/28/20.
+//  Copyright © 2020 Errin Lyton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
